ccf7cd3a7863cc34c9e5d5c3f85ee54ad716cf0f
Matthias <luethm@ethz.ch>
2025-10-19 12:59:37 +0200

    bacic buttons for tick tack toe

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App  from './assets/App.jsx'
+import "./assets/styless.css";
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
+    <App />
   </StrictMode>,
 )
