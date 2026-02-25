e0ac7b4b66075a10283ede9938f51871fae1bf1f
Spielername124 <237307027+Spielername124@users.noreply.github.com>
2026-02-25 18:00:25 +0100

    adding  dummybuttons

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import TTT  from './legacyJunk/ticTacToeFromTutorial.jsx'
 import "./style/styles.css";
 import Chess from "./assets/chess.jsx";
-
-
+import TodoApp from "./legacyJunk/toDoTutorialBackendCom.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+      <TodoApp />   {/* Hier eingefügt */}
+      <hr />
      <Chess />
   </StrictMode>,
 )
