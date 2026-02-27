1af8a178d0ccb2a5a78be189dbb0ea7d98c4f0c8
Spielername124 <237307027+Spielername124@users.noreply.github.com>
2026-02-27 09:08:11 +0100

    routing testing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import TTT  from './legacyJunk/ticTacToeFromTutorial.jsx'
 import "./style/styles.css";
-import Chess from "./assets/chess.jsx";
-import TodoApp from "./legacyJunk/toDoTutorialBackendCom.jsx";
+import Chess from "./chessIngame/assets/chess.jsx";
+import GameSelector from "./chessPregame/GameSelector.jsx";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-      <TodoApp />   {/* Hier eingefügt */}
-      <hr />
-     <Chess />
-  </StrictMode>,
-)
+    <StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<GameSelector />} />
+                <Route path="/chess" element={<Chess />} />
+            </Routes>
+        </Router>
+    </StrictMode>
+);
