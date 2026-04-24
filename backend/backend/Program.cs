72a0fd478932724675ce95a6856851a3dd79a183
Spielername124 <237307027+Spielername124@users.noreply.github.com>
2026-04-24 11:27:23 +0200

    starting to implement gamestate endpoints by bitboards

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,19 +1,30 @@
-﻿using Dapper;
+﻿using System.Data;
+using Dapper;
 using DbUp;
 using Microsoft.Extensions.Configuration;
 using System.Reflection;
+using backend.GameManagement;
+using Npgsql;
+
 
 DefaultTypeMap.MatchNamesWithUnderscores = true;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddScoped<IDbConnection>(_ => new NpgsqlConnection(connectionString));
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:5432")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
-// 3. Datenbank-Migration mit DbUp
-var connectionString = app.Configuration.GetConnectionString("DefaultConnection");
-
-// Stellt sicher, dass die Datenbank auf dem Server existiert
 EnsureDatabase.For.PostgresqlDatabase(connectionString);
 
 var upgrader = DeployChanges.To
@@ -33,5 +44,9 @@ if (!result.Successful)
 }
 
 Console.WriteLine("Database is up to date.");
+
+app.UseCors();
+
+app.MapGameInfoEndpoints();
 
 app.Run();
