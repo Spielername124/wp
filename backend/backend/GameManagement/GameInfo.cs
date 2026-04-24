33909cc37627d393effe731558c2c7b4f9dd2179
Spielername124 <237307027+Spielername124@users.noreply.github.com>
2026-04-24 11:39:39 +0200

    adding previously not added parts of the Backend

---
+++ backend/backend/GameManagement/GameInfo.cs
@@ -0,0 +1,24 @@
+﻿namespace backend.GameInfo;
+
+public class GameInfo
+{
+    public int GameId { get; set; }
+    public int? Player1Id { get; set; }
+    public int? Player2Id { get; set; }
+    public int TurnCounter { get; set; }
+    public bool HasTerminated { get; set; }
+    
+    public long WPawn { get; set; }
+    public long WKnight { get; set; }
+    public long WBishop { get; set; }
+    public long WRook { get; set; }
+    public long WQueen { get; set; }
+    public long WKing { get; set; }
+
+    public long BPawn { get; set; }
+    public long BKnight { get; set; }
+    public long BBishop { get; set; }
+    public long BRook { get; set; }
+    public long BQueen { get; set; }
+    public long BKing { get; set; }
+}
